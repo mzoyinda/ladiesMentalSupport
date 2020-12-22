bface7ad19fd807acd39de5c13cc5642b15a2321
ify110 <inwaboku@gmail.com>
2020-12-22 10:09:13 +0100

    new footer-materialize

--- src/foot.js
+++ src/foot.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import 'materialize-css';
-import {Footer} from 'react-materialize';
+import React from 'react'
+import {Footer} from 'react-materialize'
+
+const Foot = () =>{
+    return(
+        <Footer
+          className="#212121 grey darken-4"
+          copyrights="&#169; 2020 Copyright Text"
+          links={<ul><li><a className="grey-text text-lighten-3" href="#!">Home</a></li><li><a className="grey-text text-lighten-3" href="#!">About us</a></li><li><a className="grey-text text-lighten-3" href="#!">Testimonial</a></li><li><a className="grey-text text-lighten-3" href="#!">Contact us</a></li></ul>}
+          moreLinks={<a className="grey-text text-lighten-4 right" href="#!">pearlsafrica.org.ng</a>}
+          logo="pearl-logo.png" alt="pearls"
+         >
+          <div className="container">
+            <div className="row">
+              <div className="col s12 m16">
+                <h5>LadiesMentalSupport</h5>
+                <p>Our vision is to provide culturally competent, 
+                  holistic, and wellness focused services that 
+                  prevents mental health challenges</p>
+                
+
+              </div>
+
+            </div>
 
 
-
-function Foot(){
-    return (
-      <Footer
-  className="212121 grey darken-4"
-  copyrights="&copy 2015 Copyright Text"
-  links={<ul><li><a className="white-text
-  " href="#!">Lifeline</a></li><li><a className="white-text
-  " href="#!">Support</a></li><li><a className="white-text
-  " href="#!">NGOs</a></li><li><a className="white-text
-  " href="#!">MentalHealth</a></li></ul>}
-  
->
-  <h5 className="white-text">
-    MentalHealthSupport
-  </h5>
-  <p className="#212121 grey darken-4">
-    55 Moleye Street, off Herbert Macauley,
-    <p> Sabo, Yaba, Lagos.</p>
-  </p>
-  <img className="logo" src="pearl-logo.png"></img>
-</Footer>
+          </div>
+          
+        </Footer>
     )
 }
 
